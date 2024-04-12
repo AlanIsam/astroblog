afed7cda468164d83eb0f6beb6e1b30f62fac26d
Alan <2021449202@student.uitm.edu.my>
2024-04-12 22:37:35 +0800

    Initial Commit

---
+++ absent-accretion/tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,js,jsx,ts,tsx,html,css,scss,svelte,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
